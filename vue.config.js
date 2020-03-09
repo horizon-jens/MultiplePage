aefc52f3d7c19ac59a2ac3bfb24403f2572bb81d
杨遐明 <yangxiaming@yangxiamingdeMac-mini.local>
2020-03-09 14:19:36 +0800

    golb

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,37 @@
+var glob = require("glob")
+
+function getPagesInfo(){
+  let pages = {}
+  glob.sync('src/pages/**/main.js').map(entry => {
+    let title = entry.slice(10, -8)
+    pages[title] = {
+      entry,
+      template: `${entry.slice(0, -8)}/index.html`,
+      filename: `${title}.html`,
+    }
+  })
+  return pages
+}
+
+
+
 module.exports = {
-  pages: {
-    pageA: {
-      entry: 'src/pages/pageA/main.js',
-      template: 'public/index.html',
-      filename: 'pageA.html',
-      title: 'PageAAA',
-    },
-    pageB: {
-      entry: 'src/pages/pageB/main.js',
-      template: 'public/index.html',
-      filename: 'pageB.html',
-      title: 'BBB',
-    },
-  },
+  // pages: {
+  //   pageA: {
+  //     entry: 'src/pages/pageA/main.js',
+  //     template: 'src/pages/pageA/index.html',
+  //     filename: 'pageA.html',
+  //   },
+  //   pageB: {
+  //     entry: 'src/pages/pageB/main.js',
+  //     template: 'src/pages/pageB/index.html',
+  //     filename: 'pageB.html',
+  //   },
+  // },
+  pages: getPagesInfo(),
   devServer: {
     // open: true,
     // index: '/pageA.html',
-    proxy: 'http://10.0.38.93:8602/'
+    // proxy: 'http://localhost:8090/'
   }
 };
